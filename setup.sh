749c0fe3d9c34818791fee340c8de42d3d0a6276
mangupli <gaponova.liz.m@gmail.com>
2021-04-19 22:05:23 +0300

    wordpress dont work man

--- setup.sh
+++ setup.sh
@@ -4,8 +4,9 @@ eval $(minikube docker-env)
 docker build ./srcs/nginx -t nginx_image 
 kubectl apply -f ./srcs/configmap.yaml
 kubectl apply -f ./srcs/nginx/nginx.yaml
+docker build ./srcs/wordpress -t wp_image 
+kubectl apply -f ./srcs/wordpress/wp.yaml
 minikube dashboard
-
 
 
 
